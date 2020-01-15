73f81db3eb05b142fa5d3f427f5116288c26ca0d
zq7niu <740903808@qq.com>
2020-01-15 16:56:00 +0800

    add new dir

---
+++ python_tmp/20191231.py
@@ -0,0 +1,41 @@
+print(r'''hello,\n
+world''')
+from functools import reduce
+
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return DIGITS[s]
+    return reduce(fn, map(char2num, s))
+print(str2int('123'))
+
+def createCounter():
+    def counter():
+        return 1
+    return counter
+
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+def count():
+    def f(j):
+        def g():
+            return j*j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i)) # f(i)立刻被执行，因此i的当前值被传入f()
+    return fs
+
+f1, f2, f3 = count()
+print(f1())
+print(f2())
+print(f3())
