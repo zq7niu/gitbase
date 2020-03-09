482450a14626ca1e925fe334ee72aa7ee7253ec0
zq7niu <740903808@qq.com>
2020-03-09 14:51:46 +0800

    master

---
+++ python_tmp/guss_number.py
@@ -0,0 +1,13 @@
+# 该实例演示了数字猜谜游戏
+number = 7
+guess = -1
+print("Guess the number!")
+while guess != number:
+    guess = int(input("Is it... "))
+ 
+    if guess == number:
+        print("Hooray! You guessed it right!")
+    elif guess < number:         
+        print("It's bigger...")     
+    elif guess > number:
+        print("It's not so big.")
